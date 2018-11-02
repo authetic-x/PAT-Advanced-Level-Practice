e0fb5a4889bc5ea6921bbc3fc0cb82ed1124b479
authetic <1019892846@qq.com>
2018-11-02 21:56:24 +0800

    Finished Chapter 13

--- Algorithm-Note/13-Extensions/PAT-1014-Waiting_in_Line.cpp
+++ Algorithm-Note/13-Extensions/PAT-1014-Waiting_in_Line.cpp
@@ -10,6 +10,7 @@
 using namespace std;
 
 const int maxn = 1010;
+const int INF = 1000000000;
 
 struct Window {
     int endTime, popTime;
@@ -20,48 +21,47 @@ struct Window {
 int n, m, k, query, q;
 int ans[maxn], needTime[maxn];
 
-int convert(int hh, int mm) {
+int convertTime(int hh, int mm) {
     return hh * 60 + mm;
 }
 
 int main() {
     scanf("%d %d %d %d", &n, &m, &k, &query);
-    for (int i = 0; i < k; i ++ ) {
+    for (int i = 0; i < k;  i ++ ) {
         scanf("%d", &needTime[i]);
     }
     for (int i = 0; i < n; i ++ ) {
-        window[i].endTime = window[i].popTime = convert(8, 0);
+        window[i].endTime = window[i].popTime = convertTime(8, 0);
     }
     int index = 0;
-    for (int i = 0; i < min(m*n, k); i ++ ) {
+    for (int i = 0; i < min(n*m, k); i ++ ) {
         window[index % n].q.push(index);
         window[index % n].endTime += needTime[index];
-        if (index < n) window[index].popTime = needTime[index];
         ans[index] = window[index % n].endTime;
+        if (index < n) window[index % n].popTime = needTime[index];
         index++;
     }
-    for (; index < k; index++) {
-        int idx = -1, minTime = 1 << 30;
+    for (; index < k; index ++ ) {
+        int idx = -1, minTime = INF;
         for (int i = 0; i < n; i ++ ) {
             if (window[i].popTime < minTime) {
                 idx = i;
                 minTime = window[i].popTime;
             }
         }
-        Window& w = window[idx];
-        w.q.pop();
-        w.q.push(index);
-        w.endTime += needTime[index];
-        // attention +=
-        w.popTime += needTime[w.q.front()];
-        ans[index] = w.endTime;
+        window[idx].q.push(index);
+        window[idx].endTime += needTime[index];
+        ans[index] = window[idx].endTime;
+        window[idx].q.pop();
+        window[idx].popTime += needTime[window[idx].q.front()];
     }
     for (int i = 0; i < query; i ++ ) {
         scanf("%d", &q);
-        if (ans[q-1] - needTime[q-1] >= convert(17, 0)) {
+        if (ans[q-1] - needTime[q-1] >= convertTime(17, 0)) {
             printf("Sorry\n");
+        } else {
+            printf("%02d:%02d\n", ans[q-1]/60, ans[q-1]%60);
         }
-        printf("%02d:%02d\n", ans[q-1] / 60, ans[q-1] % 60);
     }
     return 0;
 }
--- Algorithm-Note/13-Extensions/PAT-1026-Table_Tennis.cpp
+++ Algorithm-Note/13-Extensions/PAT-1026-Table_Tennis.cpp
@@ -1,5 +1,12 @@
 //
 // Created by authetic on 2018/11/2.
+/*
+ * 模拟排队问题
+ * 目前花了最长时间的题目，本身不难，但是很复杂，细节处理不好就通不过
+ * 这类题目做法一般是先把用户时间整数化，按时间排序存放在vector里面；然后
+ * 将各个窗口信息存在数组里，每次找出最先空闲的窗口，算出下次结束时间
+ * 和用户等待时间， 依次遍历完vector里的用户数。
+ */
 //
 
 #include <cstdio>
@@ -15,12 +22,12 @@ const int INF = 1000000000;
 
 struct Player {
     int arriveTime, playTime, startTime;
-    bool isVip;
+    int isVip;
 }newPlayer;
 
 struct Table {
     int endTime, serveNum;
-    bool isVip;
+    int isVip;
 }table[K];
 
 vector<Player> player;
@@ -39,7 +46,7 @@ int convertTime(int h, int m, int s) {
 
 int nextVipPlayer(int vipi) {
     vipi++;
-    while (vipi < player.size() && vipi == 0) {
+    while (vipi < player.size() && player[vipi].isVip == 0) {
         vipi++;
     }
     return vipi;
@@ -57,36 +64,35 @@ void allowTable(int pId, int tId) {
 
 int main() {
     int n, k, m, vipTable;
-    scanf("%d", &n);
     int stTime = convertTime(8, 0, 0);
     int edTime = convertTime(21, 0, 0);
+    scanf("%d", &n);
     for (int i = 0; i < n; i ++ ) {
-        int hh, mm, ss, playTime, isVIP;
-        scanf("%02d:%02d:%02d %d %d", &hh, &mm, &ss
-                , &playTime, &isVIP);
+        int hh, mm, ss, playTime, isVip;
+        scanf("%d:%d:%d %d %d", &hh, &mm, &ss, &playTime, &isVip);
         newPlayer.arriveTime = convertTime(hh, mm, ss);
-        if (newPlayer.arriveTime >= edTime) continue;
-        newPlayer.startTime = edTime;
+        if (newPlayer.arriveTime > edTime) continue;
         newPlayer.playTime = playTime <= 120 ? playTime*60 : 7200;
-        newPlayer.isVip = isVIP;
+        newPlayer.isVip = isVip;
+        newPlayer.startTime = edTime;
         player.push_back(newPlayer);
     }
     scanf("%d %d", &k, &m);
-    for (int i = 0; i < k; i ++ ) {
+    for (int i = 1; i <= k; i ++ ) {
+        table[i].isVip = 0;
         table[i].endTime = stTime;
-        table[i].serveNum = table[i].isVip = 0;
+        table[i].serveNum = 0;
     }
     for (int i = 0; i < m; i ++ ) {
         scanf("%d", &vipTable);
-        table[vipTable-1].isVip = 1;
+        table[vipTable].isVip = 1;
     }
     sort(player.begin(), player.end(), cmpArriveTime);
     int i = 0, vipi = -1;
     vipi = nextVipPlayer(vipi);
-
     while (i < player.size()) {
         int idx = -1, minEndTime = INF;
-        for (int j = 0;j < k; j ++ ) {
+        for (int j = 1; j <= k; j ++ ) {
             if (table[j].endTime < minEndTime) {
                 idx = j;
                 minEndTime = table[j].endTime;
@@ -100,11 +106,11 @@ int main() {
         if (table[idx].isVip == 1) {
             if (player[i].isVip == 1) {
                 allowTable(i, idx);
-                if (vipi == i) vipi = nextVipPlayer(vipi);
+                vipi = nextVipPlayer(vipi);
                 i++;
             } else {
                 if (vipi < player.size() && player[vipi].arriveTime
-                         <= table[idx].endTime) {
+                        <= table[idx].endTime) {
                     allowTable(vipi, idx);
                     vipi = nextVipPlayer(vipi);
                 } else {
@@ -117,36 +123,36 @@ int main() {
                 allowTable(i, idx);
                 i++;
             } else {
-                int VIPidx = -1, minEndVIPTime = INF;
-                for (int j = 0; j < k; j ++ ) {
-                    if (table[j].isVip == 1 && table[j].endTime < minEndVIPTime) {
-                        VIPidx = j;
-                        minEndVIPTime = table[j].endTime;
+                int idxVIP = -1, minTime = INF;
+                for (int j = 1; j <= k; j ++ ) {
+                    if (table[j].isVip == 1 && table[j].endTime < minTime) {
+                        idxVIP = j;
+                        minTime = table[j].endTime;
                     }
                 }
-                if (VIPidx != -1 && player[i].arriveTime >= table[VIPidx].endTime) {
-                    allowTable(i, VIPidx);
-                    if (i == vipi) vipi = nextVipPlayer(vipi);
+                if (idxVIP != -1 && player[i].arriveTime >= table[idxVIP].endTime) {
+                    allowTable(i, idxVIP);
+                    vipi = nextVipPlayer(vipi);
                     i++;
                 } else {
                     allowTable(i, idx);
-                    if (vipi == i) vipi = nextVipPlayer(vipi);
+                    vipi = nextVipPlayer(vipi);
                     i++;
                 }
             }
         }
     }
     sort(player.begin(), player.end(), cmpStartTime);
-    for (i = 0; i < player.size() && player[i].startTime<edTime; i ++ ) {
+    for (i = 0; i < player.size() && player[i].startTime < edTime; i ++ ) {
         int t1 = player[i].arriveTime;
         int t2 = player[i].startTime;
         printf("%02d:%02d:%02d ", t1/3600, t1%3600/60, t1%60);
         printf("%02d:%02d:%02d ", t2/3600, t2%3600/60, t2%60);
-        printf("%.0f\n", round(t2-t1) / 60.0);
+        printf("%.0f\n", round((t2 - t1) / 60.0));
     }
-    for (i = 0; i < k; i ++ ) {
+    for (i = 1; i <= k; i ++ ) {
         printf("%d", table[i].serveNum);
-        if (i != k-1) printf(" ");
+        if (i != k) printf(" ");
     }
     return 0;
 }
